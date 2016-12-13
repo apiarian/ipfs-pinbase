0b32996c20d30f12e14e1ab2cc620406bab4c31a
Aleksandr Pasechnik <apasechnik@mediamath.com>
2016-12-12 20:01:26 -0500

    wip: start of db stuff

--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"flag"
 	"log"
+	"os/user"
+	"path/filepath"
+
+	_ "github.com/mattn/go-sqlite3"
 
 	"github.com/apiarian/go-ipfs-api"
 )
 
 func main() {
+	usr, _ := user.Current()
+
+	var (
+		dbPath = flag.String(
+			"db-path",
+			filepath.Join(usr.HomeDir, "pinbase.db"),
+			"path to the sqlite3 pin database",
+		)
+	)
+
 	s := shell.NewShell("localhost:5001")
 	if !s.IsUp() {
 		log.Fatal("IPFS API is not reachable")
