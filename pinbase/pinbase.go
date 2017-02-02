ff1b9efd10af00f98e019e6f6bce3451e02d296d
Aleksandr Pasechnik <al@megamicron.net>
2017-02-01 21:20:51 -0500

    wip: pinner stuff

--- pinbase/pinbase.go
+++ pinbase/pinbase.go
@@ -1,5 +1,9 @@
 package pinbase
 
+import (
+	"github.com/pkg/errors"
+)
+
 type Node struct {
 	Hash        string
 	Description string
@@ -28,4 +32,20 @@ type PartyStorage interface {
 	Parties() ([]*Party, error)
 	PartyForHash(hash string) (*Party, error)
 	SaveParty(*Party) error
+}
+
+type Pinner interface {
+	Pins() (map[string]struct{}, error)
+	Pin(string) error
+	Unpin(string) error
+}
+
+func StartPinManager(done <-chan struct{}, pnr Pinner) error {
+	p, err := pnr.Pins()
+	if err != nil {
+		return errors.Wrap(err, "get initial pins")
+	}
+
+	_ = p
+	return nil
 }
