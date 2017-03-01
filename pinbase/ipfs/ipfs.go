7a58c6980e1f4ce112ba2624f292690cd02845ef
Aleksandr Pasechnik <apasechnik@mediamath.com>
2017-03-01 08:45:19 -0500

    wip: ipfs pin jugggler

---
+++ pinbase/ipfs/ipfs.go
@@ -0,0 +1,29 @@
+package ipfs
+
+import (
+	"github.com/apiarian/go-ipfs-api"
+	"github.com/apiarian/ipfs-pinbase/pinbase"
+	"github.com/pkg/errors"
+)
+
+type IPFSClient struct {
+	s *shell.Shell
+}
+
+func NewIPFSClient(apiAddr string) (*IPFSClient, error) {
+	s := shell.NewShell(apiAddr)
+}
+
+func (ic *IPFSClient) Pin(pinbase.Hash) error {
+	return errors.New("not implemented")
+}
+
+func (ic *IPFSClient) Unpin(pinbase.Hash) error {
+	return errors.New("not implemented")
+}
+
+func (ic *IPFSClient) Pins() (map[pinbase.Hash]struct{}, error) {
+	return make(map[pinbase.Hash]struct{}), errors.New("not implemented")
+}
+
+var _ pinbase.PinJuggler = &IPFSClient{}
